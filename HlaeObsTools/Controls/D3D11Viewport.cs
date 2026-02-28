45ba5bcb88ad81e8e70604783ca277e559d925e3
ioctl <ioctl@mothbug.party>
2026-02-28 22:28:13 +0200

    Camera fixes

--- HlaeObsTools/Controls/D3D11Viewport.cs
+++ HlaeObsTools/Controls/D3D11Viewport.cs
@@ -531,9 +531,14 @@ public sealed class D3D11Viewport : NativeControlHost, IViewport3DControl
     public void TeleportCamera(Vector3 position, Quaternion rotation, float fov)
     {
         _freecamTransform.Position = position;
+        _freecamSmoothed.Position = position;
         _freecamTransform.Orientation = rotation;
+        _freecamSmoothed.Orientation = rotation;
         _freecamTransform.Fov = fov;
+        _freecamSmoothed.Fov = fov;
         _externalCameraActive = false;
+        _freecamActive = true;
+        RequestNextFrame();
     }
     public void SetExternalCamera(Vector3 position, Quaternion rotation, float fov)
     {
